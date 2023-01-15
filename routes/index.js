cc017bd7b2dac5d685767f7c169a368ef0329496
Fatima Zohra Bakali <bakalifatimazahrae87@gmail.com>
2023-01-15 22:53:45 +0100

    modif

--- routes/index.js
+++ routes/index.js
@@ -72,4 +72,35 @@ router.get('/titles4categ', function(req, res) {
     });
 });
 
+
+/* POST to Add User Service */
+router.post('/emprunte', function(req, res) {
+    // Set our internal DB variable
+    var db = req.db;
+    
+    // Get our form values. These rely on the "name" attributes
+    var buttonValue = req.body.buttonValue;
+
+    // Set our collection
+    var collection = db.get('mediatheque');
+
+    // Submit to the DB
+    if(buttonValue == 'Emprunter'){
+        
+    }
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");    
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
